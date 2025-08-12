ff57c3f066e1be101570e6a9b9e28cdbfc02ff5e
Rohit Dham <rdhammpls@gmail.com>
2025-08-12 11:05:24 -0500

    Init project structure

---
+++ backend/src/main/java/tech_rdham/pro/Whatsonurmind/WhatsonurmindApplication.java
@@ -0,0 +1,13 @@
+package tech_rdham.pro.Whatsonurmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsonurmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsonurmindApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tech_rdham/pro/Whatsonurmind/WhatsonurmindApplicationTests.java
@@ -0,0 +1,13 @@
+package tech_rdham.pro.Whatsonurmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsonurmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
